c0b92f65b3e6bfc2d15d05b4fdfad12af89e5acf
pandafulmanda <pandafulmanda@gmail.com>
2016-10-07 23:02:42 -0500

    make imports more consistent, add some quick comments

--- simple-gpiozero.py
+++ simple-gpiozero.py
@@ -1,34 +1,40 @@
-from gpiozero import *
-from time import sleep
+# get packages to help us
+import gpiozero
+import time
 
-relay_4 = OutputDevice(22)
-relay_3 = OutputDevice(27)
-relay_2 = OutputDevice(17)
-relay_1 = OutputDevice(4)
+# set up each of the relay pins as outputs
+relay_1 = gpiozero.OutputDevice(4)
+relay_2 = gpiozero.OutputDevice(17)
+relay_3 = gpiozero.OutputDevice(27)
+relay_4 = gpiozero.OutputDevice(22)
 
+# toggle relay 1!
 print('Turn on 1!')
 relay_1.on()
-sleep(1)
+time.sleep(1)
 print('Turn off 1!')
 relay_1.off()
+time.sleep(1)
 
-sleep(1)
+# toggle relay 2!
 print('Turn on 2!')
 relay_2.on()
-sleep(1)
+time.sleep(1)
 print('Turn off 2!')
 relay_2.off()
+time.sleep(1)
 
-sleep(1)
+# toggle relay 3!
 print('Turn on 3!')
 relay_3.on()
-sleep(1)
+time.sleep(1)
 print('Turn off 3!')
 relay_3.off()
+time.sleep(1)
 
-sleep(1)
+# toggle relay 4!
 print('Turn on 4!')
 relay_4.on()
-sleep(1)
+time.sleep(1)
 print('Turn off 4!')
 relay_4.off()
