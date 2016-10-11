c325f67ddb6c7987f1f27e3a4bf7b4d9fb68f5a0
pandafulmanda <pandafulmanda@gmail.com>
2016-10-11 00:16:15 -0500

    add timeout

--- simple-gpiozero.py
+++ simple-gpiozero.py
@@ -11,30 +11,30 @@ relay_4 = gpiozero.OutputDevice(22)
 # toggle relay 1!
 print('Turn on 1!')
 relay_1.on()
-time.sleep(1)
+time.sleep(10)
 print('Turn off 1!')
 relay_1.off()
-time.sleep(1)
+time.sleep(10)
 
 # toggle relay 2!
 print('Turn on 2!')
 relay_2.on()
-time.sleep(1)
+time.sleep(10)
 print('Turn off 2!')
 relay_2.off()
-time.sleep(1)
+time.sleep(10)
 
 # toggle relay 3!
 print('Turn on 3!')
 relay_3.on()
-time.sleep(1)
+time.sleep(10)
 print('Turn off 3!')
 relay_3.off()
-time.sleep(1)
+time.sleep(10)
 
 # toggle relay 4!
 print('Turn on 4!')
 relay_4.on()
-time.sleep(1)
+time.sleep(10)
 print('Turn off 4!')
 relay_4.off()
